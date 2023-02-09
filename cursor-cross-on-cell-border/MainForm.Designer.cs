54b12cc212e3921b5cb54d962c69300bf7577114
Tom Gregor <tgregor@ivsoftware.com>
2023-02-09 15:42:18 -0700

    RevA

--- cursor-cross-on-cell-border/MainForm.Designer.cs
+++ cursor-cross-on-cell-border/MainForm.Designer.cs
@@ -37,11 +37,13 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.tableLayoutPanel.CellBorderStyle = System.Windows.Forms.TableLayoutPanelCellBorderStyle.Single;
-            this.tableLayoutPanel.ColumnCount = 4;
+            this.tableLayoutPanel.ColumnCount = 3;
             this.tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
             this.tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
             this.tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
             this.tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.tableLayoutPanel.ForeColor = System.Drawing.Color.DarkSlateGray;
             this.tableLayoutPanel.Location = new System.Drawing.Point(25, 25);
             this.tableLayoutPanel.Name = "tableLayoutPanel";
             this.tableLayoutPanel.RowCount = 4;
@@ -49,14 +51,14 @@
             this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
             this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
             this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
-            this.tableLayoutPanel.Size = new System.Drawing.Size(528, 494);
+            this.tableLayoutPanel.Size = new System.Drawing.Size(428, 494);
             this.tableLayoutPanel.TabIndex = 0;
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(578, 544);
+            this.ClientSize = new System.Drawing.Size(478, 544);
             this.Controls.Add(this.tableLayoutPanel);
             this.Name = "MainForm";
             this.Padding = new System.Windows.Forms.Padding(25);
--- cursor-cross-on-cell-border/MainForm.cs
+++ cursor-cross-on-cell-border/MainForm.cs
@@ -12,35 +12,29 @@ namespace cursor_cross_on_cell_border
             // TableLayoutPanel has CROSS
             tableLayoutPanel.Cursor = Cursors.Cross;
 
+            int key = 0; string text;
             for (int column = 0; column < tableLayoutPanel.ColumnCount; column++)
                 for (int row = 0; row < tableLayoutPanel.RowCount; row++)
                 {
-                    tableLayoutPanel.Controls.Add(new Panel
+                    switch (++key)
+                    {
+                        case 10: text = "*"; break;
+                        case 11: text = "0"; break;
+                        case 12: text = "#"; break;
+                        default: text = $"{key}";  break;
+                    }
+                    tableLayoutPanel.Controls.Add(new Label
                     {
                         BackColor = Color.LightGreen,
                         Anchor = (AnchorStyles)0xF,
-                        Margin = new Padding(10),
+                        Margin = new Padding(10),  
+                        Text = text,
+                        TextAlign = ContentAlignment.MiddleCenter,
 
                         // Controls in the table have HAND
                         Cursor = Cursors.Hand,
                     });
                 }
-        }
-
-        int _wdtCount = 0;
-        private void onResized(object? sender, EventArgs e)
-        {
-            var capture = ++_wdtCount;
-            Task
-                .Delay(2500)
-                .GetAwaiter()
-                .OnCompleted(() =>
-                {
-                    if (capture.Equals(_wdtCount))
-                    {
-
-                    }
-                });
         }
     }
 }
